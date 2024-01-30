125a72a6df5d39c6034a431ae03efbec265f0d5e
Qutaiba Qusai Makahleh <qutaiba.makahleh@gmail.com>
2024-01-30 10:13:03 +0300

    change the name of css file

--- main.js
+++ main.js
@@ -1,19 +1,4 @@
-
-
-
 let prevScrollPos = window.pageYOffset;
-
-window.onscroll = function() {
-    let currentScrollPos = window.pageYOffset;
-
-    if (prevScrollPos > currentScrollPos) {
-        document.querySelector("header").style.top = "0";
-    } else {
-        document.querySelector("header").style.top = "-80px"; // Adjust this value based on the height of your header
-    }
-
-    prevScrollPos = currentScrollPos;
-};
 
 // Function to scroll smoothly to the top
 function scrollToTop() {
@@ -23,19 +8,28 @@ function scrollToTop() {
     });
 }
 
-// Function to toggle the visibility of the back-to-top button
-function toggleBackToTopButton() {
+// Function to toggle the visibility of the back-to-top button and header
+function handleScroll() {
+    let currentScrollPos = window.pageYOffset;
+    let header = document.querySelector("header");
     var backToTopButton = document.getElementById('backToTopBtn');
 
-    // If the user has scrolled down, show the button; otherwise, hide it
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    // Toggle header visibility
+    if (prevScrollPos > currentScrollPos) {
+        header.style.top = "0";
+    } else {
+        header.style.top = "-80px"; // Adjust this value based on the height of your header
+    }
+
+    // Toggle back-to-top button visibility
+    if (currentScrollPos > 20) {
         backToTopButton.style.display = 'block';
     } else {
         backToTopButton.style.display = 'none';
     }
+
+    prevScrollPos = currentScrollPos;
 }
 
-// Attach the toggleBackToTopButton function to the scroll event
-window.onscroll = function() {
-    toggleBackToTopButton();
-};
+// Attach the handleScroll function to the scroll event
+window.onscroll = handleScroll;
