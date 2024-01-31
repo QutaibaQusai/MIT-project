04563806e7fde05b1e53670b31e19b60e432992f
Qutaiba Qusai Makahleh <qutaiba.makahleh@gmail.com>
2024-01-31 22:01:11 +0300

    nav bar responsive and add some project

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 let prevScrollPos = window.pageYOffset;
 
-// Function to scroll smoothly to the top
 function scrollToTop() {
     window.scrollTo({
         top: 0,
@@ -8,20 +7,17 @@ function scrollToTop() {
     });
 }
 
-// Function to toggle the visibility of the back-to-top button and header
 function handleScroll() {
     let currentScrollPos = window.pageYOffset;
     let header = document.querySelector("header");
     var backToTopButton = document.getElementById('backToTopBtn');
 
-    // Toggle header visibility
     if (prevScrollPos > currentScrollPos) {
         header.style.top = "0";
     } else {
-        header.style.top = "-80px"; // Adjust this value based on the height of your header
+        header.style.top = "-80px"; 
     }
 
-    // Toggle back-to-top button visibility
     if (currentScrollPos > 20) {
         backToTopButton.style.display = 'block';
     } else {
@@ -31,5 +27,6 @@ function handleScroll() {
     prevScrollPos = currentScrollPos;
 }
 
-// Attach the handleScroll function to the scroll event
 window.onscroll = handleScroll;
+
+
